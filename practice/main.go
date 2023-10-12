2a031d2df6cf1619424e8de9a8bcffca2e2fd254
Tanil Cadette <tanil.cadette@sap.com>
2023-10-12 14:45:51 -0400

    completed

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Structs")
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	poodle.Weight = 9
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+}
+
+type Dog struct {
+	Breed  string
+	Weight int
 }
