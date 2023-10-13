b62d7c1fa358d61b22c33c9642937e2a8ea55425
Tanil Cadette <tanil.cadette@sap.com>
2023-10-13 11:26:50 -0400

    complete

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,28 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+}
+
+func doSomething() {
+	fmt.Println("Doing Something")
+	sum := addValues(10, 34)
+	fmt.Println("The sum is:", sum)
+
+	multiSum, multiCount := addAllValues(1, 5, 43, 4, -9)
+	fmt.Println("The sum of multiple values:", multiSum)
+	fmt.Println("The count of multiple count:", multiCount)
+
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0
+	for _, v := range values {
+		total += v
+	}
+	return total, len(values)
 }
