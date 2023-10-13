cd895a763cc845fa136fa1c31c64973fb4da78a3
Tanil Cadette <tanil.cadette@sap.com>
2023-10-13 11:47:11 -0400

    complete

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,30 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "woof!"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+	poodle.Speak()
+	poodle.Sound = "Arf!!"
+	poodle.Speak()
+	poodle.speakThreeTimes()
+	poodle.speakThreeTimes()
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+// Speak is how the dog speaks
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+func (d Dog) speakThreeTimes() {
+	d.Sound = fmt.Sprintf("%v %v %v\n", d.Sound, d.Sound, d.Sound)
+	fmt.Print(d.Sound)
 }
